22ba14bdff8ca62eb33e28c0adf5d626728324c5
Naseem <naseemhammoud@gmail.com>
2016-11-17 14:45:34 -0800

    Created base sales page and controller, added comments finished job #1, minus fatal error at welcome.php, need major help Fixed final issue, job one done Jobs 1-8 Done Signed-off-by:Naseem <naseemhammoud@gmail.com>

--- application/controllers/Shopping.php
+++ application/controllers/Shopping.php
@@ -9,10 +9,24 @@ class Shopping extends Application
 		parent::__construct();
 	}
 	
-	public function index()
-	{
-        $stuff = file_get_contents('../data/receipt.md');
-        $this->data['receipt'] = $this->parsedown->parse($stuff);
+        public function index() {
+    // What is the user up to?
+    if ($this->session->has_userdata('order'))
+        $this->keep_shopping();
+    else $this->summarize();
+}
+
+public function summarize() {
+    $this->data['pagebody'] = 'summary';
+    $this->render('template');  // use the default template
+}
+
+public function keep_shopping() {
+      $order = new Order($this->session->userdata('order'));
+      $stuff = $order->receipt();
+      $this->data['receipt'] = $this->parsedown->parse($stuff);
+    //$stuff = file_get_contents('../data/receipt.md');
+        //$this->data['receipt'] = $this->parsedown->parse($stuff);
 		$this->data['content'] = '';
 		
 		// pictorial menu
@@ -27,6 +41,35 @@ class Shopping extends Application
 			$count++;
 		}
 		$this->render('template-shopping'); 
-	}
+    
+}
 
+public function add($what) {
+    //$order = $this->session->userdata('order');
+    $order = new Order($this->session->userdata('order'));
+    $order->additem($what);
+    $this->keep_shopping();
+    $this->session->set_userdata('order',(array)$order);
+}
+
+public function neworder() {
+    // create a new order if needed
+    if (! $this->session->has_userdata('order')) {
+        $order = new Order();
+        //$this->session->set_userdata('order',$order);
+        $this->session->set_userdata('order', (array) $order);
+    }
+
+    $this->keep_shopping();
+}
+
+public function cancel() {
+    // Drop any order in progress
+    if ($this->session->has_userdata('order')) {
+        $this->session->unset_userdata('order');
+    }
+
+    $this->index();
+}
+	
 }
--- application/views/template-shopping.php
+++ application/views/template-shopping.php
@@ -24,6 +24,9 @@
 							{category3}
 						</div>
 					</div>
+                                     <div class=row">
+                                         <a class="btn btn-default" role="button" href="/shopping/cancel">Cancel This Order</a>
+                                     </div>
 				</div>
 				<div class='col-md-3'>
 					{receipt}
